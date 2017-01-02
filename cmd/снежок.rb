7f8b227e2986dca07dded451de51af95222cafc0
Nickolay <nickolay02@inbox.ru>
2017-01-03 00:05:02 +0200

    Ибо балда.

--- cmd/снежок.rb
+++ cmd/снежок.rb
@@ -34,3 +34,11 @@ oncmd(/снежкотлета (.+)/) do |m, data|
   y = [$rus_throwtargets.sample, "промахнулся"].sample
   m.bot.a_action m.sendto, "взял снежок и котлету, смял в единое целое, кинул #{target} и #{y}."
 end
+
+CmdHelp.new "rus", "пнутьбалду", "пнутьбалду [цель]", "Пинает балду"
+oncmd(/пнутьбалду/) do |m, data|
+  m.bot.a_action m.sendto, "пнул #{m.nick}. Ибо балда."
+end
+oncmd(/пнутьбалду (.+)/) do |m, data|
+  m.bot.a_action m.sendto, "пнул #{data[1].squish}. Ибо балда."
+end
