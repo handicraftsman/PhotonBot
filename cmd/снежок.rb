10d39708d15244369d852b1bc9ed90ecc0b628d7
Nickolay <nickolay02@inbox.ru>
2017-01-02 23:39:13 +0200

    Updated version number

--- cmd/снежок.rb
+++ cmd/снежок.rb
@@ -1,15 +1,35 @@
 CmdHelp.new "rus", "снежок", "снежок <цель>", "Швыряет снежок в цель"
-oncmd(/снежок (.+)/) do |m, data|
-  x = [
-    "руку",
-    "ногу",
-    "глаз",
-    "нос",
-    "рот",
-    "пятую точку",
-    "пах",
-    "голову (headshot!)",
-    "астрал"
-  ].sample
-  m.bot.a_action m.sendto, "кинул снежком в #{data[1].squish} и попал в #{x}."
+$rus_throwtargets = [
+  "руку",
+  "ногу",
+  "глаз",
+  "нос",
+  "рот",
+  "пятую точку",
+  "пах",
+  "голову (headshot!)",
+]
+
+$rus_throwtargets.map! do |i|
+  "попал в #{i}"
+end
+
+oncmd(/снежок (.+)/) do |m, data|  
+  if data[1].squish == "всех"
+    target = "во всех"
+  else
+    target = "в " + data[1].squish
+  end
+  y = [$rus_throwtargets.sample, "промахнулся"].sample
+  m.bot.a_action m.sendto, "кинул снежком #{target} и #{y}."
+end
+
+oncmd(/снежкотлета (.+)/) do |m, data|
+  if data[1].squish == "всех"
+    target = "во всех"
+  else
+    target = "в " + data[1].squish
+  end
+  y = [$rus_throwtargets.sample, "промахнулся"].sample
+  m.bot.a_action m.sendto, "взял снежок и котлету, смял в единое целое, кинул #{target} и #{y}."
 end
